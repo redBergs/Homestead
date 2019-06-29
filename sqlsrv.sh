b8f120335623cc55435b9495235022d22eaccf9c
redBergs <22152253+redBergs@users.noreply.github.com>
2019-06-29 09:28:22 -0400

    Create sqlsrv.sh
    This is to install MS SQL server, the ODBC Driver and SQL Command Line Utility, and Microsoft SQL Server for Laravel/Homestead

---
+++ sqlsrv.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+clear
+
+echo "Install MS SQL on Ubuntu Server 18.04";
+echo "Apt update";
+echo "+--------------------------------------------------------------+";
+sudo apt-get update
+
+echo "Import the necessary repository key";
+echo "+--------------------------------------------------------------+";
+sudo wget -qO- https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+
+echo "Add the repository";
+echo "+--------------------------------------------------------------+";
+sudo add-apt-repository "$(wget -qO- https://packages.microsoft.com/config/ubuntu/16.04/mssql-server-preview.list)"
+
+echo "Update APT";
+echo "+--------------------------------------------------------------+";
+sudo apt-get update
+
+sleep 5
+
+echo "Install MS SQL Server";
+echo "+--------------------------------------------------------------+";
+sudo apt-get install -y mssql-server
+
+echo "Configure MS SQL Server //MsSql@123";
+echo "+--------------------------------------------------------------+";
+sudo /opt/mssql/bin/mssql-conf setup
+
+systemctl status mssql-server
+
+echo "Restart the MS SQL server";
+echo "+--------------------------------------------------------------+";
+systemctl restart mssql-server.service
+
+sleep 5
+
+echo "Register the Microsoft Linux repositories and add their keys";
+echo "+--------------------------------------------------------------+";
+sudo su <<EOF
+curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
+curl https://packages.microsoft.com/config/ubuntu/18.04/prod.list > /etc/apt/sources.list.d/mssql-release.list
+EOF
+
+echo "Install the ODBC Driver and SQL Command Line Utility for SQL Server";
+echo "+--------------------------------------------------------------+";
+sudo apt-get update
+sudo ACCEPT_EULA=Y apt-get install msodbcsql17 mssql-tools
+echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bash_profile
+echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bashrc
+source ~/.bashrc
+sudo apt-get install unixodbc-dev
+
+sleep 5
+# sqlcmd -S localhost -U sa -P MsSql@123 -Q "SELECT @@VERSION"
+
+echo "Install sqlsrv";
+echo "+--------------------------------------------------------------+";
+sudo pecl install sqlsrv
+
+echo "Install pdo_sqlsrv";
+echo "+--------------------------------------------------------------+";
+sudo pecl install pdo_sqlsrv
+
+echo "Install the PHP drivers for Microsoft SQL Server";
+echo "+--------------------------------------------------------------+";
+sudo su <<EOF
+echo "extension=sqlsrv.so" >> "/etc/php/7.3/fpm/php.ini"
+echo "extension=pdo_sqlsrv.so" >> "/etc/php/7.3/fpm/php.ini"
+
+echo "extension=sqlsrv.so" >> "/etc/php/7.3/cli/php.ini"
+echo "extension=pdo_sqlsrv.so" >> "/etc/php/7.3/cli/php.ini"
+/etc/init.d/php7.3-fpm restart 
+EOF
